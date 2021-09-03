74d8179a143396c0fa39eb09d1b8d565579a01f2
fikerte <tofikertesfaye@gmail.com>
2021-09-03 12:37:35 +0300

    Fix linter errors

--- formvalidation.js
+++ formvalidation.js
@@ -1,34 +1,30 @@
 const email = document.querySelector('.email');
 const errorMsg = document.getElementById('errorMsg');
 const submitBtn = document.querySelector('.submit-button');
+let valid = 'false';
 
 function checkEmailValidity() {
   if (email.value === '') {
     errorMsg.innerText = 'Email can not be empty';
     email.classList.add('invalid-email');
     email.classList.remove('valid-email');
-    return false;
+  } else if (email.value !== email.value.toLowerCase()) {
+    errorMsg.innerText = 'Email should be in lowercase';
+    email.classList.add('invalid-email');
+    email.classList.remove('valid-email');
   } else {
     errorMsg.innerText = '';
     email.classList.add('valid-email');
     email.classList.remove('invalid-email');
+    valid = 'true';
   }
-  if (email.value !== email.value.toLowerCase()) {
-    errorMsg.innerText = 'Email should be in lowercase';
-    email.classList.add('invalid-email');
-    email.classList.remove('valid-email');
-    return false;
-  } else {
-    email.classList.add('valid-email');
-    email.classList.remove('invalid-email');
-  }
+  return valid;
 }
 email.addEventListener('input', checkEmailValidity);
 
 submitBtn.addEventListener('click', (e) => {
-  // alert('hello');
   e.preventDefault();
-  if (checkEmailValidity()) {
+  if (valid) {
     submitBtn.submit();
   }
 });
